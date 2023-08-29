942d35a2e074d62ba38ce53d28ce41c9a85225e3
BorzaAndrei <andreiborza98@gmail.com>
2023-08-30 00:03:42 +0300

    Fixed total progress and some more

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,16 +1,43 @@
-import React from "react";
-import { Button, ScrollView } from "react-native";
-import Title from "../components/Title";
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StyleSheet, Button, ScrollView, View, Alert } from "react-native";
 import Countdown from "../components/Countdown";
+import { initialChapters, loadChapters } from "../models/InitialChapters";
+import TotalProgress from "../components/TotalProgress";
+import { useIsFocused } from '@react-navigation/native';
 
 
 const HomeScreen = ({navigation}: {navigation: any}) => {
+
+    const [chapters, setChapters] = useState(initialChapters);
+    const isFocused = useIsFocused();
+
+    useEffect(() => {
+        loadChapters(setChapters);
+    }, [isFocused])
+
+    function resetData() {
+        AsyncStorage.removeItem('chapters');
+    }
+
     return (
         <ScrollView>
             <Countdown />
-            <Button title='Details' onPress={() => navigation.navigate('ChaptersScreen')}/>
+            <TotalProgress chapters={chapters}/>
+            <View style={styles.button}>
+                <Button title='Chapters' onPress={() => navigation.navigate('ChaptersScreen')}/>
+            </View>
+            <View style={styles.button}>
+                <Button title='Reset Data' onPress={resetData}/>
+            </View>
         </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    button: {
+        marginTop: '3%'
+    }
+})
 
 export default HomeScreen
